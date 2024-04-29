62230b1c8b7bed53b8bcf8f575e4ccecce43b0ff
loverikim <loverikim03@gmail.com>
2024-04-29 15:34:20 +0900

    자바 기본문법 배우기

---
+++ src/helloworld_20240429/Hello.java
@@ -0,0 +1,14 @@
+package helloworld_20240429;
+
+public class Hello { // 중괄호 열고 닫고
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld!!");
+		System.out.println("안녕하세요!!");		
+		// 콘솔에 결과를 프린트 -> 주의: 자바는 ""만 문자열에 사용
+		// main 함수가 있어야지만 실행 시킬 수 있다.
+		// 파이썬과 다르게 문법 마지막은 ;(세미콜론)으로 끝남.
+	}
+
+}
