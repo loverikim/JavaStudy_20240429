d6dae1b127bc9d7fbee0d574068f70325bf4e633
loverikim <loverikim03@gmail.com>
2024-04-29 16:20:15 +0900

    깃허브 수정 올리기

--- src/helloworld_20240429/Hello.java
+++ src/helloworld_20240429/Hello.java
@@ -6,6 +6,7 @@ public class Hello { // 중괄호 열고 닫고
 		// TODO Auto-generated method stub
 		System.out.println("Helloworld!!");
 		System.out.println("안녕하세요!!");		
+		System.out.println("깃허브에 올릴 내용 수정");	
 		// 콘솔에 결과를 프린트 -> 주의: 자바는 ""만 문자열에 사용
 		// main 함수가 있어야지만 실행 시킬 수 있다.
 		// 파이썬과 다르게 문법 마지막은 ;(세미콜론)으로 끝남.
